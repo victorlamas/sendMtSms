2d4133f98cc5637f91664d080e762a0bca5941e6
Nvia <50366052+victorlamas@users.noreply.github.com>
2024-03-20 16:43:44 +0100

    Update sendMTSms.bat

--- Windows batch/sendMTSms.bat
+++ Windows batch/sendMTSms.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set "url=https://www.panelsms.com/httpinput/sendMtSms.php"
 
 :: JSON parameters
-set "data={\"user\":\"your_api_user\",\"apikey\":\"your_api_key\",\"to\":\"recipient\",\"message\":\"your_message\"}"
+set "data={\"user\":\"your_api_user\",\"apiKey\":\"your_api_key\",\"to\":\"recipient\",\"message\":\"your_message\"}"
 
 :: Perform the HTTP POST request using PowerShell
 powershell -Command "& { $response = Invoke-RestMethod -Uri '%url%' -Method Post -Body '%data%' -ContentType 'application/json'; Write-Output $response }"
