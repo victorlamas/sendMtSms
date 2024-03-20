feb0982a655a52066442c6f272b82eddd50344bc
Nvia <50366052+victorlamas@users.noreply.github.com>
2024-03-20 16:44:42 +0100

    Update sendMTSms_TempFileJson.bat

--- Windows batch/sendMTSms_TempFileJson.bat
+++ Windows batch/sendMTSms_TempFileJson.bat
@@ -2,13 +2,12 @@
 
 set "url=https://www.panelsms.com/httpinput/sendMtSms.php"
 set "user=your_api_user"
-set "apikey=your_api_key"
+set "apiKey=your_api_key"
 set "recipient=recipient"
 set "message=your_message"
 
 :: Construct JSON data
-set "jsonData={"user":"%user%","apiKey":"%apikey%","to":"%recipient%","message":"%message%"}"
-
+set "jsonData={"user":"%user%","apiKey":"%apiKey%","to":"%recipient%","message":"%message%"}"
 :: Save JSON data to a temporary file
 echo %jsonData% > temp.json
 
